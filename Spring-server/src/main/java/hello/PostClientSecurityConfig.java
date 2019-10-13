47510011db212abcebbe1d1ccc41e643e6973c14
IReallyHateLogins <deathstingray@gmial.com>
2019-10-14 02:45:37 +0300

    Changed from tomcat to undertow
    Moving to undertow due to tomcat producing an annoying exeption in log on every request, and all keystorage problems on tomcat 9.0.25+

--- Spring-server/src/main/java/hello/PostClientSecurityConfig.java
+++ Spring-server/src/main/java/hello/PostClientSecurityConfig.java
@@ -1,12 +1,13 @@
 package hello;
 
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import io.undertow.UndertowOptions;
+import io.undertow.servlet.api.SecurityConstraint;
+import io.undertow.servlet.api.SecurityInfo;
+import io.undertow.servlet.api.TransportGuaranteeType;
+import io.undertow.servlet.api.WebResourceCollection;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,19 +15,20 @@ import org.springframework.context.annotation.Configuration;
 public class PostClientSecurityConfig {
     @Bean
     public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
+        UndertowServletWebServerFactory undertow = new UndertowServletWebServerFactory();
+        undertow.addBuilderCustomizers(builder -> {
+            builder.addHttpListener(httpPort, "0.0.0.0");
+            builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
+        });
+        undertow.addDeploymentInfoCustomizers(deploymentInfo -> {
+            deploymentInfo.addSecurityConstraint(new SecurityConstraint()
+                    .addWebResourceCollection(new WebResourceCollection()
+                            .addUrlPattern("/*"))
+                    .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
+                    .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
+                    .setConfidentialPortManager(exchange -> httpsPort);
+        });
+        return undertow;
     }
 
     @Value("${server.port.http}")
@@ -34,14 +36,4 @@ public class PostClientSecurityConfig {
 
     @Value("${server.port}")
     int httpsPort;
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(httpPort);
-        connector.setSecure(false);
-        connector.setRedirectPort(httpsPort);
-
-        return connector;
-    }
 }
