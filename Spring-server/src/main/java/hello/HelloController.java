a1efb2d087af36afe9737f09770275eb7f1e1d98
IReallyHateLogins <IReallyHateLogins@users.noreply.github.com>
2019-10-17 20:49:53 +0300

    Fixed formatting

--- Spring-server/src/main/java/hello/HelloController.java
+++ Spring-server/src/main/java/hello/HelloController.java
@@ -19,72 +19,73 @@ import org.springframework.http.HttpStatus;
 @RestController
 public class HelloController {
 
-	private final UserRepository repository;
+    private final UserRepository repository;
 
-	HelloController(UserRepository repository) {
-		this.repository = repository;
-	}
+    HelloController(UserRepository repository) {
+        this.repository = repository;
+    }
 
-  @ExceptionHandler(ResponseStatusException.class)
-  @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "No such user")
-   String handleNotFoundException(ResponseStatusException ex) {
-    return ex.toString();
-  }
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "No such user")
+    String handleNotFoundException(ResponseStatusException ex) {
+        return ex.toString();
+    }
 
-  @GetMapping("/users/{id}")
-  @ResponseStatus(HttpStatus.OK)
-  User uniqueUser(@PathVariable Long id) {
-    return repository.findById(id)
-      .orElseThrow(
-        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No such user"));
-  }
+    @GetMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    User uniqueUser(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No such user"));
+    }
 
-  @GetMapping("/users")
-  @ResponseStatus(HttpStatus.OK)
-  	List<User> all() {
-    	return repository.findAll();
-  	}
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    List<User> all() {
+        return repository.findAll();
+    }
 
 
-	@PostMapping("/users")
-  @ResponseStatus(HttpStatus.CREATED)
-	User newUser(@RequestBody User newUser) {
-  	return repository.save(newUser);
-	}
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    User newUser(@RequestBody User newUser) {
+        return repository.save(newUser);
+    }
 
- 	@PutMapping("/users/{id}")
-  @ResponseStatus(HttpStatus.OK)
- 	User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
-    return repository.findById(id)
-      .map(user -> {
-        user.setLogin(newUser.getLogin());
-        user.setPassword(newUser.getPassword());
-        user.setMailLogin(newUser.getMailLogin());
-        user.setMailPassword(newUser.getMailPassword());
-        user.setSmtpAddr(newUser.getSmtpAddr());
-        user.setSmtpPort(newUser.getSmtpPort());
-        user.setImapAddr(newUser.getImapAddr());
-        user.setImapPort(newUser.getImapPort());
+    @PutMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(user -> {
+                    user.setLogin(newUser.getLogin());
+                    user.setPassword(newUser.getPassword());
+                    user.setMailLogin(newUser.getMailLogin());
+                    user.setMailPassword(newUser.getMailPassword());
+                    user.setSmtpAddr(newUser.getSmtpAddr());
+                    user.setSmtpPort(newUser.getSmtpPort());
+                    user.setImapAddr(newUser.getImapAddr());
+                    user.setImapPort(newUser.getImapPort());
 
-        return repository.save(user);
-      })
-      .orElseThrow(
-        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No such user"));
-  	}
+                    return repository.save(user);
+                })
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No such user"));
+    }
 
     @DeleteMapping("/users")
     @ResponseStatus(HttpStatus.NOT_FOUND)
     void deleteAllUsers() {
-      // It's forbidden to delete the whole collection
+        // It's forbidden to delete the whole collection
     }
 
-  	@DeleteMapping("/users/{id}")
+    @DeleteMapping("/users/{id}")
     @ResponseStatus(HttpStatus.OK)
-  	void deleteUser(@PathVariable Long id) {
+    void deleteUser(@PathVariable Long id) {
         if (repository.existsById(id)) {
-          repository.deleteById(id);  
+            repository.deleteById(id);
         } else {
-          throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such user");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such user");
         }
-  	}
+    }
+
 }
