fbf16db1ca25ae56570496862270adebd247e9b5
Daniel1998 <79062860832@yandex.ru>
2019-10-17 23:00:29 +0300

    Upgrade PostMessage class.
    Added fields numberInFolder, recipients folder.
    Added correctly return of multipart content.

--- Spring-server/src/main/java/hello/PostMessage.java
+++ Spring-server/src/main/java/hello/PostMessage.java
@@ -1,31 +1,43 @@
 package hello;
 
-import javax.mail.Address;
-import javax.mail.Message;
-import javax.mail.MessagingException;
+import javax.mail.*;
+import javax.mail.internet.MimeMultipart;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class PostMessage {
+    private int numberInFolder;
     private String title;
-    private String content;
+    private Object content;
     private String contentType;
     private Date date;
-    private Address[] addresses;
+    private Address[] recivers;
+    private Address[] recipients;
+    private String folder;
 
-    public PostMessage(String title, String content, String contentType, Date date, Address[] addresses) {
+
+
+
+    public PostMessage(String title, String content, String contentType, Date date, Address[] recivers, Address[] recipients, Folder folder) {
         this.title = title;
         this.content = content;
         this.contentType = contentType;
         this.date = date;
-        this.addresses = addresses;
+        this.recivers = recivers;
+        this.recipients = recipients;
+        this.folder = folder.getFullName();
     }
     public PostMessage(Message message) throws MessagingException, IOException {
         this.title = message.getSubject();
-        this.content = message.getContent().toString();
+        this.content = message.getContent();
         this.contentType = message.getContentType();
+        this.numberInFolder = message.getMessageNumber();
         this.date = message.getSentDate();
-        this.addresses = message.getFrom();
+        this.recivers = message.getFrom();
+        this.recipients = message.getAllRecipients();
+        this.folder = message.getFolder().getFullName();
     }
 
     public String getTitle() {
@@ -36,11 +48,41 @@ public class PostMessage {
         this.title = title;
     }
 
-    public String getContent() {
+    public Object getContent() {
+
+        if (this.contentType.contains("text") || this.contentType.contains("html"))
+        {
+            return content.toString();
+        }
+        if (this.contentType.contains("multipart"))
+        {
+            List<Object> ret = new ArrayList<>();
+            MimeMultipart mimeMultipart = (MimeMultipart) content;
+            BodyPart bodyPart;
+            int count = 0;
+            try {
+                count = mimeMultipart.getCount();
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i<count; i++)
+            {
+                try {
+                    bodyPart = mimeMultipart.getBodyPart(i);
+                    ret.add(bodyPart.getContent());
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return ret;
+        }
+
         return content;
     }
 
-    public void setContent(String content) {
+    public void setContent(Object content) {
         this.content = content;
     }
 
@@ -60,11 +102,36 @@ public class PostMessage {
         this.date = date;
     }
 
-    public Address[] getAddresses() {
-        return addresses;
+    public Address[] getRecivers() {
+        return recivers;
     }
 
-    public void setAddresses(Address[] addresses) {
-        this.addresses = addresses;
+    public void setRecivers(Address[] addresses) {
+        this.recivers = addresses;
+    }
+
+    public int getNumberInFolder() {
+        return numberInFolder;
+    }
+
+    public void setNumberInFolder(int numberInFolder) {
+        this.numberInFolder = numberInFolder;
+    }
+
+
+    public Address[] getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(Address[] recipients) {
+        this.recipients = recipients;
+    }
+
+    public String getFolder() {
+        return folder;
+    }
+
+    public void setFolder(String folder) {
+        this.folder = folder;
     }
 }
