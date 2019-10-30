bc199a68278397d5e672130caa51bb4e3cd2ee79
Поликарп <snowzhop@gmail.com>
2019-10-31 00:56:32 +0300

    Added configuration classes for JWT-auth

---
+++ Spring-server/src/main/java/ikpi63holding/postclient/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package ikpi63holding.postclient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        
+        // Grab credentials and map them to login viewmodel
+        LoginViewModel credentials = null;
+        try {
+            credentials = new ObjectMapper().readValue(request.getInputStream(), LoginViewModel.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Create login token
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                credentials.getUsername(),
+                credentials.getPassword(),
+                new ArrayList<>());
+
+        // Authenticate user
+        Authentication auth = authenticationManager.authenticate(authenticationToken);
+
+        return auth;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        
+        UserPrincipal principal = (UserPrincipal) authResult.getPrincipal();
+
+        String token = JWT.create()
+                .withSubject(principal.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(JwtProperties.SECRET.getBytes())); // TODO Remember it
+
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX +  token);
+    }
+}
---
+++ Spring-server/src/main/java/ikpi63holding/postclient/JwtProperties.java
@@ -0,0 +1,8 @@
+package ikpi63holding.postclient;
+
+public class JwtProperties {
+    public static final String SECRET = "ikpi63HOLDING";
+    public static final int EXPIRATION_TIME = 864000000;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+}
