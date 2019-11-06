e8ceefb60c8e891f062b2191ddb95b406869f484
Darya Lobanova <loisering@gmail.com>
2019-11-06 22:15:34 +0300

    Added registration form Changed some elements of auth form Added ability to switch btw sign in/sign up forms

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -3,10 +3,6 @@ var password = null;
 var shoPass = 0;
 var remMe = 0;
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('checkLabel').className = 'input-checkbox-label far fa-square';
-});
-
 function saveUserData() {
     username = document.getElementById('user').value;
     password = document.getElementById('pass').value;
@@ -16,12 +12,6 @@ function saveUserData() {
 }
 
 function login() {
-    if (document.getElementById('user').value === null) {
-        console.log('enter username');
-    } else if (document.getElementById('pass').value === null) {
-        console.log('enter password');
-    }
-    alert('username: ' + username + '\npassword: ' + password)
 }
 
 function goToPage() {
@@ -30,27 +20,73 @@ function goToPage() {
   document.location.href = url;
 }
 
-function showPass() {
+function showPass1() {
     if (shoPass === 0) {
-        document.getElementById('pass').type = 'text';
+        document.getElementById('pass1').type = 'text';
         document.getElementById('eye-icon').className = 'fas fa-eye';
         shoPass = 1;
     } else {
-        document.getElementById('pass').type = 'password';
+        document.getElementById('pass1').type = 'password';
         document.getElementById('eye-icon').className = 'fas fa-eye-slash';
         shoPass = 0;
     }
 }
 
-function rememberMe() {
-    if (remMe === 1) {
-        document.getElementById('checkLabel').className = 'input-checkbox-label far fa-square';
-        console.log('dont remember');
-        remMe = 0;
+function showPass2() {
+    if (shoPass === 0) {
+        document.getElementById('pass2').type = 'text';
+        document.getElementById('eye-icon').className = 'fas fa-eye';
+        shoPass = 1;
     } else {
-        document.getElementById('checkLabel').className = 'input-checkbox-label fas fa-check';
-        console.log('remember');
-        remMe = 1;
-        saveUserData();
+        document.getElementById('pass2').type = 'password';
+        document.getElementById('eye-icon').className = 'fas fa-eye-slash';
+        shoPass = 0;
     }
+}
+
+function rememberMe() {    
+    let check = document.getElementById('check');
+    let icon = document.getElementById('icon');   
+
+    if (check.checked === false) {
+        icon.className = 'far fa-check-square';
+        check.checked = true;
+        
+    } else {
+        icon.className = 'far fa-square';
+        check.checked = false;
+    }
+}
+
+function toSignIn() {
+    let labelIn = document.getElementById('sign-in');
+    let labelUp = document.getElementById('sign-up');
+    let btn = document.getElementById('btn');
+    let confirm = document.getElementById('pass2');
+    let confirmField = document.getElementById('conf');
+    let footer = document.getElementById('hide');
+ 
+    //19c125
+    labelIn.className = 'auth on';
+    labelUp.className = 'auth off';
+    btn.textContent = 'sign in';
+    confirm.style.display = 'none';
+    confirmField.style.display = 'none';
+    footer.style.display = 'flex';
+}
+
+function toSignUp() {
+    let labelIn = document.getElementById('sign-in');
+    let labelUp = document.getElementById('sign-up');
+    let btn = document.getElementById('btn');
+    let confirm = document.getElementById('pass2');
+    let confirmField = document.getElementById('conf');
+    let footer = document.getElementById('hide');
+
+    labelIn.className = 'auth off';
+    labelUp.className = 'auth on';
+    btn.textContent = 'sign up';
+    confirm.style.display = 'block';
+    confirmField.style.display = 'block';
+    footer.style.display = 'none';
 }
