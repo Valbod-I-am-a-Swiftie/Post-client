90ac02785016c8ef0546ff4638792b06ef95b6a4
IReallyHateLogins <IReallyHateLogins@users.noreply.github.com>
2019-11-08 12:32:21 +0300

    Added new storage for folders, removed old option
    New storade is based on set and map for storing key values. Database format left untouched. Added custom serializer and deserializer, plus some helper classes for them, might rework later. Standart serializers don't like unwrapping this mess of a structure. Old folders removed.

---
+++ Spring-server/src/main/java/ikpi63holding/postclient/MailFolderSetDeserializer.java
@@ -0,0 +1,37 @@
+package ikpi63holding.postclient;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MailFolderSetDeserializer extends StdDeserializer<MailFolderSet> {
+
+    public MailFolderSetDeserializer() {
+        this(null);
+    }
+
+    public MailFolderSetDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public MailFolderSet deserialize(JsonParser jp, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException {
+
+        JsonNode root = jp.getCodec().readTree(jp);
+        List<MailFolderJsonObject> jsonObjectList = new ArrayList<>();
+        for (JsonNode node: root)
+        {
+            MailFolderJsonObject jsonObject = new MailFolderJsonObject();
+            jsonObject.setName(node.get("name").asText());
+            jsonObject.setType(node.get("type").asText());
+            jsonObjectList.add(jsonObject);
+        }
+        return new MailFolderSet(jsonObjectList);
+    }
+}
