090a4c64aa21b4f3f9cac552c83e29f3ba049180
IReallyHateLogins <IReallyHateLogins@users.noreply.github.com>
2019-11-11 11:56:50 +0300

    User id changed to UUID, added query methods for username
    Also removed unnecessary setters/getters, they are created by lombok

--- Spring-server/src/main/java/ikpi63holding/postclient/data/user/User.java
+++ Spring-server/src/main/java/ikpi63holding/postclient/data/user/User.java
@@ -2,73 +2,65 @@ package ikpi63holding.postclient.data.user;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import ikpi63holding.postclient.data.maibox.Mailbox;
-import java.util.List;
-import javax.persistence.Column;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.CascadeType;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 @Data
 @Entity
-public class User {
+public class User implements Serializable {
+
     @Id
-    @GeneratedValue(
-        strategy= GenerationType.AUTO, 
-        generator="native"
-    )
-    private Long id;
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, insertable = false, nullable = false)
+    private UUID id;
 
     @Column(nullable = false)
     private String username;
 
     @Column(nullable = false)
     private String password;
- 
+
     private int active = 1;
 
     private String roles = "";
 
     private String permissions = "";
-  
-    @OneToMany(mappedBy = "user", cascade=CascadeType.ALL)
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonManagedReference
     private List<Mailbox> mailboxes;
 
     public void addMailbox(Mailbox mailbox) {
-        this.mailboxes.add(mailbox);
+        int num = mailboxes.size();
         mailbox.setUser(this);
+        mailbox.setId(num + 1);
+        this.mailboxes.add(mailbox);
     }
 
-    // New methods for auth
-    public int getActive() {
-        return active;
-    }
-
-    public String getRoles() {
-        return roles;
-    }
-
-    public String getPermissions() {
-        return permissions;
-    }
-
-    public List<String> getRoleList(){
-        if(this.roles.length() > 0){
+    public List<String> getRoleList() {
+        if (this.roles.length() > 0) {
             return Arrays.asList(this.roles.split(","));
         }
         return new ArrayList<>();
     }
 
-    public List<String> getPermissionList(){
-        if(this.permissions.length() > 0){
+    public List<String> getPermissionList() {
+        if (this.permissions.length() > 0) {
             return Arrays.asList(this.permissions.split(","));
         }
         return new ArrayList<>();
     }
+
 }
--- Spring-server/src/main/java/ikpi63holding/postclient/data/user/UserRepository.java
+++ Spring-server/src/main/java/ikpi63holding/postclient/data/user/UserRepository.java
@@ -1,9 +1,16 @@
 package ikpi63holding.postclient.data.user;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String userName);
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    void deleteByUsername(String username);
+
 }
