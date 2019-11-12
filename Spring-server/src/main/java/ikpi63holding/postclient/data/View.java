6f99a065ab108157b2498d6bab836b09da2a2819
IReallyHateLogins <IReallyHateLogins@users.noreply.github.com>
2019-11-12 21:50:06 +0300

    Added json view to entities

---
+++ Spring-server/src/main/java/ikpi63holding/postclient/data/View.java
@@ -0,0 +1,26 @@
+package ikpi63holding.postclient.data;
+
+public class View {
+
+    // compact is used to mark properties showed in lists
+    public interface Compact {}
+
+    // following views are used to mark properties shown in single object
+    public interface User extends Compact {}
+    public interface Mailbox extends Compact {}
+    public interface Folder extends Compact {}
+
+    // following views is used to mark private info to be available only when adding new entities
+    public interface NewUser extends User {}
+    public interface NewMailbox extends Mailbox {}
+    public interface NewFolder extends Folder {}
+
+    // following views mark properties to be shown only in according admin mode
+    public interface AdminCompact extends Compact {}
+    public interface UserAdmin extends User, AdminCompact{}
+    public interface MailboxAdmin extends Mailbox, AdminCompact{}
+    public interface FolderAdmin extends Folder, AdminCompact{}
+    public interface NewUserAdmin extends NewUser, UserAdmin {}
+    public interface NewMailboxAdmin extends NewMailbox, MailboxAdmin {}
+    public interface NewFolderAdmin extends NewFolder, FolderAdmin {}
+}
--- Spring-server/src/main/java/ikpi63holding/postclient/data/maibox/Mailbox.java
+++ Spring-server/src/main/java/ikpi63holding/postclient/data/maibox/Mailbox.java
@@ -1,6 +1,8 @@
 package ikpi63holding.postclient.data.maibox;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonView;
+import ikpi63holding.postclient.data.View;
 import ikpi63holding.postclient.data.folders.MailFolderSet;
 import ikpi63holding.postclient.data.user.User;
 import javax.persistence.Column;
@@ -20,27 +22,35 @@ import lombok.Setter;
 public class Mailbox {
     @Id
     @Column(updatable = false, insertable = false, nullable = false)
+    @JsonView(View.Compact.class)
     private int id;
 
     @Column(nullable = false)
+    @JsonView(View.Compact.class)
     private String mailLogin;
 
     @Column(nullable = false)
+    @JsonView(View.NewMailbox.class)
     private String mailPassword;
 
     @Column(nullable = false)
+    @JsonView(View.Mailbox.class)
     private String smtpAddr;
 
     @Column
+    @JsonView(View.Mailbox.class)
     private int smtpPort;
 
     @Column(nullable = false)
+    @JsonView(View.Mailbox.class)
     private String imapAddr;
 
     @Column
+    @JsonView(View.Mailbox.class)
     private int imapPort;
 
     @Column
+    @JsonView(View.Mailbox.class)
     @Setter(AccessLevel.NONE)
     @Getter(AccessLevel.NONE)
     private String folders = "";
